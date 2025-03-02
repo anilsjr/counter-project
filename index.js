8dbfada2b45d6e47d93c214a4c30d6484fde3dc6
Anil Vishwakarma <anil.vishwakarma.it@gmail.com>
2025-03-02 20:30:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const btnEl = document.getElementById("btn");
-const emojiNameEl = document.getElementById("emoji-name");
+let num = 0;
 
-const emoji = [];
+const value = document.querySelector(".value");
+const btns = document.querySelectorAll(".btn");
 
-async function getEmoji() {
-  let response = await fetch(
-    "https://emoji-api.com/emojis?access_key=773b58f681fb786fafdb8392e8b8a75ddc177fd1"
-  );
-
-  data = await response.json();
-
-  for (let i = 0; i < 1500; i++) {
-    emoji.push({
-      emojiName: data[i].character,
-      emojiCode: data[i].unicodeName,
-    });
-  }
-}
-
-getEmoji();
-
-btnEl.addEventListener("click", () => {
-  const randomNum = Math.floor(Math.random() * emoji.length);
-  btnEl.innerText = emoji[randomNum].emojiName;
-  emojiNameEl.innerText = emoji[randomNum].emojiCode;
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      num--;
+    } else if (styles.contains("increase")) {
+      num++;
+    } else {
+      num = 0;
+    }
+    value.textContent = num;
+    if (num > 0) {
+      value.style.color = "green";
+    } else if (num < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "black";
+    }
+  });
 });
